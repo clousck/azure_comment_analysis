df3f633c9031f85d4b3a60f48a546973bf65fbdc
Alan Sandoval <alansandovalproject@gmail.com>
2025-01-04 21:54:20 -0500

    API file

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+from library.use_model import preprocesar_texto, predecir_sentimiento
+
+app = Flask(__name__)
+
+# Ruta de prueba
+@app.route('/test', methods=['GET'])
+def hello_world():
+    texto = "Me encantó este producto, es excelente"
+    preprocesado = preprocesar_texto(texto)
+    print(preprocesado)
+    sentimiento = predecir_sentimiento(texto)
+    print(sentimiento)
+
+    return jsonify({'message': sentimiento})
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    # Obtener el texto desde el formulario
+    texto = request.form.get('comentario', '')
+
+    # Llamar a la función para predecir el sentimiento
+    prediction = predecir_sentimiento(texto)
+    return jsonify(prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
